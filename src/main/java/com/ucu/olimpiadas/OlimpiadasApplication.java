ef85934d81a41cbf5913c955dc7c4d9caa3ed8c9
Matías Varela <matias.varela@scanntech.com>
2024-06-18 16:42:19 -0300

    Te movi toh capoh

---
+++ src/main/java/com/ucu/olimpiadas/OlimpiadasApplication.java
@@ -0,0 +1,13 @@
+package com.ucu.olimpiadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlimpiadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlimpiadasApplication.class, args);
+	}
+
+}

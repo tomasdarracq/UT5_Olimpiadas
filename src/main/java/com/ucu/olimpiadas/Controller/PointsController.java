49cc58407ba8446846b56b16f50a337a3af9e133
LeonSalvo <110829744+LeonSalvo@users.noreply.github.com>
2024-06-19 11:00:32 -0300

    point service

--- src/main/java/com/ucu/olimpiadas/Controller/PointsController.java
+++ src/main/java/com/ucu/olimpiadas/Controller/PointsController.java
@@ -1,4 +1,14 @@
 package com.ucu.olimpiadas.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/points")
 public class PointsController {
+
+    @RequestMapping("/calculate")
+    public void calculatePoints() {
+
+    }
 }
